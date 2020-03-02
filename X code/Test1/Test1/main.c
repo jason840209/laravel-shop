2908387ceec6e51a9935c6082a0dd41d27ac93ff
jason840209 <key9889131@gmail.com>
2020-03-02 17:24:38 +0800

    test
    it is test

---
+++ X code/Test1/Test1/main.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  Test1
+//
+//  Created by jasonlin on 2019/7/12.
+//  Copyright © 2019年 jasonlin. All rights reserved.
+//
+
+#include <stdio.h>
+class Car{
+    public String brand;
+    public String type;
+    public int hp;
+    public String getInformation(){
+        return "廠牌"+brand+"型號"+type+"馬力"+hp;
+    }
+}
+Car BMW = new Car("BMW","X5",340);//宣告類別為Car的資料，並給定BMW變數
+Car Porsche = new Car("Porsche","Cayenne",300);//宣告類別為Car的資料，並給定Porsche變數
+
+BMW.getInformation();//印出資訊
+Porsche.getInformation();//印出資訊
